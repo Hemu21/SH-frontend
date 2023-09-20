4df6ee924b42bed03a97b49f3587e777c1b0f23c
Hemanth kumar <rajva5727@gmail.com>
2023-09-20 14:40:48 +0530

    Update UserVideo.jsx

--- src/main/VideoCall/UserVideo.jsx
+++ src/main/VideoCall/UserVideo.jsx
@@ -7,14 +7,14 @@ export default function UserVideo() {
   const url_slice = _url.slice(_url.lastIndexOf("chat/"))
   const user_id = url_slice.slice(5,url_slice.indexOf("/u"))
   const id = _url.slice(_url.lastIndexOf("/")+1)
-  const backendURL = process.env.BACKENDURL
+  const backendURL = process.env.BACKENDURL || "https://sh-backend-61my.onrender.com"
   const [idData,setIdData] = useState()
   const [idToCall, setIdToCall] = useState('');
 
   const {name,setName, callAccepted,me, myVideo, userVideo, callEnded, stream,answerCall, callUser } = useContext(SocketContext);  
   
   useEffect( ()=>{
-    fetch(`${backendURL}/users`).then((res)=>res.json().then((ele)=>ele.map((ee)=>{if(ee._id===id+"data"){ setIdData(ee)};if(ee._id===id){setIdToCall(ee.call_id)}})))
+    fetch(`${backendURL}/api/v1/users`).then((res)=>res.json().then((ele)=>ele.map((ee)=>{if(ee._id===id+"data"){ setIdData(ee)};if(ee._id===id){setIdToCall(ee.call_id)}})))
     setName(user_id);
 
     
