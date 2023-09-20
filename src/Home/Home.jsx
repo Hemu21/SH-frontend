8f0013023baaf889c0ca5626ee15907c0b3fec41
Hemanth kumar <rajva5727@gmail.com>
2023-09-20 15:20:24 +0530

    Update Home.jsx

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 import Button from '@mui/material/Button';
 
 export default function Home() {
-  const image = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgoZDXHeBmkqH64BEKXjpbi3kayNosBtOh4aZWs-louMnFB2l7s474BU0nwG-BWR5zbITpZIdqkTfJ0eMagdLhcu9AgVOJbbKxD5LJ5Z828latEQJ0iOTQZHQBzyERiHqdsPraVshZkQOF9omAzVq_w58QvzZ_PqGbnUjzX_ec5jzJ4rJgAH9qDq61m0t4/s320/image1.png")
+  const image = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgoZDXHeBmkqH64BEKXjpbi3kayNosBtOh4aZWs-louMnFB2l7s474BU0nwG-BWR5zbITpZIdqkTfJ0eMagdLhcu9AgVOJbbKxD5LJ5Z828latEQJ0iOTQZHQBzyERiHqdsPraVshZkQOF9omAzVq_w58QvzZ_PqGbnUjzX_ec5jzJ4rJgAH9qDq61m0t4/s320/image1.png"
     
   return (
     <div>
